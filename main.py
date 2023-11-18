6fbffc901b708cb297411a760a49da8e377c5d9b
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-11-18 22:51:38 +0800

    Deleted Virtual Environment and Added new OptionMenu

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import customtkinter
 import cryptography
 import base64
 
+'''
+$ TODO $
+## NOTE: Each of these functionality na ig aadd must have the capability to be reversed
+# Add SHA-2 (SHA-224, SHA-256, SHA-384, SHA-512)
+# Add SHA-3 (SHA3-224, SHA3-256, SHA3-384, SHA3-512)
+# Add MD5
+# Add BLAKE2 
+# Add AES (Advanced Encryption Standard) in various modes (CBC, GCM, CFB, etc.)
+# Add Fernet (a symmetric encryption implementation using a combination of AES and HMAC)
+# Add RSA (Rivest-Shamir-Adleman)
+# Add DSA (Digital Signature Algorithm)
+# Add Elliptic Curve Cryptography (ECC) algorithms (ECDSA, ECDH)
+# RSA signatures
+# DSA signatures
+# Binary to Text
+'''
 class App(customtkinter.CTk):
 
     encryptionAlgorithms = {
@@ -21,14 +37,20 @@ class App(customtkinter.CTk):
         self.plaintext_Entry = customtkinter.CTkTextbox(self, width=400, corner_radius=5)
         self.plaintext_Entry.grid(row=1, column=0, padx=20, columnspan=2, pady=15)
 
-        ##### Buttons #####
-        self.encrypt_or_decrypt = customtkinter.CTkOptionMenu(self, values=["Encrypt", "Decrypt"], command=self.execute)
-        self.encrypt_or_decrypt.grid(row=2, column=0)
+        ##### 
+        self.recipie = customtkinter.CTkOptionMenu(self, values=["Hash Functions", "Symmetric Encryption", "Asymmetric Encryption", "Digital Signatures"], command=self.displayRecipie)
+        self.recipie.grid(row=2, column=0)
+        
         self.encryptionAlgoToUse = customtkinter.CTkOptionMenu(self, values=[
             "Base64", 
             ""
         ])
-        self.encryptionAlgoToUse.grid(row=2, column=1)
+
+        self.encryptionAlgoToUse.grid(row=2, column=3)
+
+        ##### Pick either to Encrypt to Decrypt #####
+        self.encrypt_or_decrypt = customtkinter.CTkOptionMenu(self, values=["Encrypt", "Decrypt"], command=self.execute)
+        self.encrypt_or_decrypt.grid(row=2, column=1)
 
         ##### Encrypted text output #####
         self.encryptedText_Entry = customtkinter.CTkTextbox(self, width=400, corner_radius=5)
